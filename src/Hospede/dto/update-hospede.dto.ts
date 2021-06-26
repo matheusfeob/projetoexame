e3fae8847442a3237b75d76c8168f97a1e6a0b1a
matheusfeob <matheus.augusto@sou.unifeob.edu.br>
2021-06-26 13:11:56 -0300

    Revert "entity hospede"
    This reverts commit d1b3ca8d3173f625b4f5d60faecf725ecd2f5c4d.

--- src/Hospede/dto/update-hospede.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHospedeDto } from './create-hospede.dto';
-
-export class UpdateHospedeDto extends PartialType(CreateHospedeDto) {}
